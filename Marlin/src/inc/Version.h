e1b9cdc506e2593abc72131c51eb808087864e20
Christopher Norton <christopher@securly.com>
2020-07-19 20:46:43 -0400

    Update version and compiled binary

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "2.0.5.4 (Magic v0.3.2)" // CNorton
+  #define SHORT_BUILD_VERSION "2.0.5.4 (Magic v0.3.3)" // CNorton
 #endif
 
 /**
@@ -42,7 +42,7 @@
  * version was tagged.
  */
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2020-07-15" // CNorton
+  #define STRING_DISTRIBUTION_DATE "2020-07-19" // CNorton
 #endif
 
 /**
