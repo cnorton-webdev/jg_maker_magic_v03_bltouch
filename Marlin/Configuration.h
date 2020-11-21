254b774d4541722199490215e7b017c3b7bc91be
Christopher Norton <christopher@securly.com>
2020-11-21 12:16:16 -0500

    Enable SD card support

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1657,7 +1657,7 @@
  * you must uncomment the following option or it won't work.
  *
  */
-//#define SDSUPPORT
+#define SDSUPPORT
 
 /**
  * SD CARD: SPI SPEED
@@ -1674,7 +1674,7 @@
  *
  * Use CRC checks and retries on the SD communication.
  */
-//#define SD_CHECK_AND_RETRY
+#define SD_CHECK_AND_RETRY
 
 /**
  * LCD Menu Items
